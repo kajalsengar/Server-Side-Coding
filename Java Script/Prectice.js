b5558213ff22b61c00417daa9f0b0d21f08e389d
kajal sengar <sengarkajal089@gmail.com>
2025-07-30 00:37:44 +0530

    prectice

---
+++ Java Script/Prectice.js
@@ -0,0 +1,16 @@
+ const Profile = {
+    userName : "kajal sengar",
+    follower: 412,
+    following: 79,
+    isFollow: true,
+    post:9,
+    bio:"Prem Shanti Anandshach"
+ };
+
+console.log(Profile);
+ console.log(Profile.bio);
+
+ Profile["post"]= 11;
+ console.log(Profile["post"]);
+
+
