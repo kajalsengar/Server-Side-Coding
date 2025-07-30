a60965414bb240b4632ff3a4bfcba298ed9fee65
kajal sengar <sengarkajal089@gmail.com>
2025-07-30 23:33:14 +0530

    assignment operators

---
+++ Java Script/Operators/assignmentOperators.js
@@ -0,0 +1,20 @@
+let a =5;
+let b =2;
+let c ;
+
+c=a;
+
+c = a ** b;
+console.log(c);
+
+console.log("a = 5+2 =>", a += b);
+console.log("a = 7-2 =>", a -= b);
+console.log("a = 5*2 =>", a *= b);
+console.log("a = 10/2=>", a /= b);
+console.log("a = 5%2 =>", a %= b);
+console.log("a = 1**2 =>", a**=b);
+
+console.log(c<<=a);
+console.log(c >>= a);
+
+
---
+++ Java Script/Operators/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a % b =", a % b);
+console.log("a ** b =", a ** b);
