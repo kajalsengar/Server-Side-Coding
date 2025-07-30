52c350ef52103a8790e267fabf5d6713d7276aa4
kajal sengar <sengarkajal089@gmail.com>
2025-07-30 22:54:38 +0530

    opeators

---
+++ Java Script/Operators/airthmeticOperators.js
@@ -0,0 +1,16 @@
+let a = 5;
+let b = 2;
+
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a % b =", a % b);
+console.log("a ** b =", a ** b);
+
+//Unary Operators 
+console.log("a++ =", a++);
+console.log(a);
+
+console.log("--b =", --b);
+console.log(b);
