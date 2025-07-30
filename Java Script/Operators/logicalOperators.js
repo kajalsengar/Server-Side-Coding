f9ea74a28507395647e380f5780d2a34b2ff2ffb
kajal sengar <sengarkajal089@gmail.com>
2025-07-31 00:25:19 +0530

    logical operator

---
+++ Java Script/Operators/logicalOperators.js
@@ -0,0 +1,11 @@
+let a = 5;
+let b = 10;
+let c = 15;
+
+
+let e = a > b && b < c;
+console.log("5 > 10 && 10 < 15 ",e);
+let d = a > b || b < c;
+console.log("5 > 10 || 10 < 15 ", d);
+console.log(!d);
+console.log(!e);
