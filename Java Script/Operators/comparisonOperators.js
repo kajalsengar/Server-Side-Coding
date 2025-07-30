04d4714a1b4ce09cd002ea060c139d2dae0d855e
kajal sengar <sengarkajal089@gmail.com>
2025-07-30 23:56:16 +0530

    assignment operators

---
+++ Java Script/Operators/comparisonOperators.js
@@ -0,0 +1,12 @@
+let a = 10;
+let b = '10';
+let c = 5;
+
+console.log("10 == 10(String)",a == b); 
+console.log("10 === 10(String)",a === b);  
+console.log("10 != 5",a != c);   
+console.log("10 !== 10(String)",a !== b);  
+console.log("10 > 5",a > c);    
+console.log("5 < 10",c < a);    
+console.log("10 >= 10",a >= 10);  
+console.log("5 <= 5",c <= 5);   
