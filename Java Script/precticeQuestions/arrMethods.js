fb46798aeea2bd0eb9601c5aeb02f889fba74083
kajal sengar <sengarkajal089@gmail.com>
2025-08-02 23:14:35 +0530

    array method

---
+++ Java Script/precticeQuestions/arrMethods.js
@@ -0,0 +1,13 @@
+let arr = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+console.log(arr);
+
+let newArr = arr.shift();
+console.log(newArr);
+console.log(arr);
+
+let arr1 = arr.splice(1,1,"Ola");
+console.log(arr1);
+console.log(arr);
+
+arr.push("Amazon");
+console.log(arr);
--- Java Script/precticeQuestions/createUserName.js
+++
@@ -1,4 +0,0 @@
- let fullName = prompt("Enter your fullname without space..");
-
- let userName = "@" + fullName + fullName.length;
- console.log("Your username is = ",userName);
