22b0862001e810b99f83f093bb00893ce99b1468
kajal sengar <sengarkajal089@gmail.com>
2025-08-04 22:58:47 +0530

    count vowels in string

--- Java Script/precticeQuestions/countVowels.js
+++ Java Script/precticeQuestions/countVowels.js
@@ -9,5 +9,18 @@ function vowelCount (str) {
     console.log(str);
   console.log(count);
 }
+vowelCount("Radha Krishna");
 
-vowelCount("Radha Krishna")
+
+const vowCount = (str) => {
+    let count = 0;
+      for(const char of str){
+        if(char ==="a" || char ==="i" || char ==="e" || char==="o" || char==="u" ){
+            count++;
+        }
+      }
+    console.log(str);
+      return count;
+}
+
+vowCount("Radha Krishna");
