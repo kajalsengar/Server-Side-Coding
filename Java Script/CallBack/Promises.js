3e8498ce1e0a0c7560e2f61531859e9b7a74e55b
kajal sengar <sengarkajal089@gmail.com>
2025-08-14 00:29:58 +0530

    promise chaining

--- Java Script/CallBack/Promises.js
+++ Java Script/CallBack/Promises.js
@@ -1,42 +1,52 @@
-function asyncFunc1(){
-return new Promise ((Resolve, Reject)=>{
-setTimeout(() => {
-    console.log("some data1");
-    Resolve("success");
-}, 3000);
-});
-}
+// function asyncFunc1(){
+// return new Promise ((Resolve, Reject)=>{
+// setTimeout(() => {
+//     console.log("some data1");
+//     Resolve("success");
+// }, 3000);
+// });
+// }
 
-function asyncFunc2() {
-   return new Promise  ((Resolve, Reject) => {
-        setTimeout(() => {
-            console.log("some data2");
-            Resolve("success");
-        }, 3000);
-    });
-}
+// function asyncFunc2() {
+//    return new Promise  ((Resolve, Reject) => {
+//         setTimeout(() => {
+//             console.log("some data2");
+//             Resolve("success");
+//         }, 3000);
+//     });
+// }
 
-console.log("fetching data1...");
-asyncFunc1().then((res) => {
-    console.log("fetching data2...");
-    asyncFunc2().then((res) => {
-        console.log(res);
-    })
+// console.log("fetching data1...");
+// asyncFunc1().then((res) => {
+//     console.log("fetching data2...");
+//     asyncFunc2().then((res) => {
+//         console.log(res);
+//     })
 
-})
+// })
 
 
 //Promise chaining
 
 
-// function getData(dataId, getNextData) {
-//   return new Promise((Resolve ,Reject)=>{
-//       setTimeout(() => {
-//         Reject("error")
-//           console.log( "data " ,dataId );
-//         if (getNextData) {
-//             getNextData();
-//         }
-//       }, 2000);
-//   })
-// }
+function getData(dataId) {
+  return new Promise((Resolve ,Reject)=>{
+      setTimeout(() => {
+        // Reject("error");
+        Resolve("success !")
+          console.log( "data",dataId);
+      }, 3000);
+  })
+}
+
+console.log("Getting data 1.")
+getData(1).then((res)=>{
+    console.log("Getting data 2..")
+   return getData(2);
+}).then((res)=>{
+    console.log("Getting data 3...")
+    return getData(3);
+    }).then((res)=>{
+        console.log(res)
+    })
+
