e02f3777df9616dfa94389c1de773de86474ea10
kajal sengar <sengarkajal089@gmail.com>
2025-08-14 23:12:32 +0530

    asycn await

--- Java Script/CallBack/asyncAwait.js
+++ Java Script/CallBack/asyncAwait.js
@@ -1,13 +1,20 @@
-function api() {
+function getData(dataId){
     return new Promise ((resolve , reject)=>{
-       setTimeout(() => {
-           console.log("weather data");
-           resolve(200);
-       }, 2000);
+        setTimeout(() => {
+            console.log("data", dataId);
+            resolve(200);
+        }, 2000);
     })
 }
 
-async function getWeatherApi() {
-    await api();
-    await api();
+
+async function getAllData() {
+    console.log("getting data 1.");
+    await getData(1);
+    console.log("getting data 2..");
+    await getData(2);
+    console.log("getting data 3...");
+    await getData(3);
+    console.log("getting data 4....");
+    await getData(4);
 }
