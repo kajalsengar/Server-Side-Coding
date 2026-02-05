22bf712e20d97d97d6a8677362bb8d7ab757b5c5
Kajal Sengar <sengarkajal089@gmail.com>
2026-02-05 12:25:45 +0530

    IIFEs function

--- Common JS/functions/calculatorFun.js
+++ Common JS/functions/calculatorFun.js
@@ -5,6 +5,10 @@ function calculator (a,b,operator){
     if(operator=='/') return a/b;
 }
 
+(function(){
+    console.log(5+100);
+})();
+
 console.log(calculator(4,5,"+"));
 console.log(calculator(4,5,"*"));
 console.log(calculator(4,5,"-"));
